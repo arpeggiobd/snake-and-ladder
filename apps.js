6e26fb2dcdaf3cfbdb9a728f3064bd9dafb84cf0
arpeggio <wongjl.chester@gmail.com>
2022-08-11 15:19:14 +0800

    added  3files

---
+++ apps.js
@@ -0,0 +1,22 @@
+
+
+const randomNum = () => {
+    const dice = 6
+    const roll = Math.ceil(Math.random() * dice);
+    console.log("You rolled", dice)
+    return dice
+}
+
+const rollDice = () => {
+    $('button').on('click', () => {
+    })
+}
+
+
+
+const main = () => {
+
+}
+
+$(main)
+
