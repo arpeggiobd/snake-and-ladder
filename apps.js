4a73df51975bb052c443a7fc546f97af88aae6bd
arpeggio <wongjl.chester@gmail.com>
2022-08-11 16:01:25 +0800

    Rendering Board

--- apps.js
+++ apps.js
@@ -1,21 +1,49 @@
 
 
+// dice rolling
 const randomNum = () => {
     const dice = 6
     const roll = Math.ceil(Math.random() * dice);
-    console.log("You rolled", dice)
-    return dice
+    console.log("You rolled", roll)
+    return roll
 }
 
+// roll dice on click
 const rollDice = () => {
-    $('button').on('click', () => {
-    })
+
+}
+
+// Rendering board
+const width = 10; // Width of board
+const height = 10; // Height of board
+const board = [];
+
+for (let y = 0; y < height; y++) {
+    let row = [];
+    board.push(row);
+    for (let x = 0; x < width; x++) {
+        row.push({ x, y, occupied: null })
+    }
+}
+
+console.log(board)
+
+const boardSize = 50; // size of each square
+const renderBoard = () => {
+    let boardHTML = ``;
+    board.forEach(row => {
+        row.forEach(square => {
+            boardHTML += `<div class = square style ="top: ${square.y * boardSize}px; left:${square.x * boardSize}px"></div>`
+        });
+    });
+    document.getElementById("board").innerHTML = boardHTML;
+    console.log("Render Board");
 }
 
 
 
 const main = () => {
-
+    renderBoard();
 }
 
 $(main)
